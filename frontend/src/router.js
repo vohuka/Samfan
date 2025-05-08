3357c25b3fd684cca8f74b159dbfd144844bf52f
Lorce <huynhphatloc2004@gmail.com>
2025-05-08 10:16:28 +0700

    New page in About (BusinessAreas.vue)

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -12,6 +12,7 @@ const About = () => import('./components/About.vue')
 const Product = () => import('./components/Product.vue')
 const Contact = () => import('./components/Contact.vue')
 const FAQ = () => import('./components/FAQ.vue')
+const BusinessAreas = () => import('./components/BusinessAreas.vue')
 
 const routes = [
   {
@@ -58,6 +59,11 @@ const routes = [
     path: '/product/:id', // Thêm route mới cho trang chi tiết sản phẩm
     name: 'ProductDetail',
     component: ProductDetail
+  },
+  {
+    path: '/business-areas',
+    name: 'BusinessAreas',
+    component: BusinessAreas
   },
 
 ]
